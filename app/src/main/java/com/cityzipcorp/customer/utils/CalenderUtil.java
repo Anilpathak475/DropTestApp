1ddb366c2c12fc1bd4c6b45d088dc524ee37e115
anilpathak <anilpathak475@gmail.com>
2018-05-08 14:15:33 +0530

    removes unused methods

--- app/src/main/java/com/cityzipcorp/customer/utils/CalenderUtil.java
+++ app/src/main/java/com/cityzipcorp/customer/utils/CalenderUtil.java
@@ -21,16 +21,12 @@ public class CalenderUtil {
 
     private static String dateFormat = "yyyy-MM-dd";
 
-    public static String getISO8601DateStringFromDate(Date date) {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ENGLISH);
-        return df.format(date);
-    }
-
     public static String getTime(Date date) {
         String dateFormat = "hh:mm a";
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
         return dateFormatter.format(date);
     }
+
     public static String getAMPMValueFromDate(Date date) {
         String dateFormat = "a";
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
@@ -42,6 +38,7 @@ public class CalenderUtil {
         SimpleDateFormat dateFormatter = new SimpleDateFormat(formatDate, Locale.ENGLISH);
         return dateFormatter.format(date);
     }
+
     public static String getMonthAndDate(Date date) {
         String dateFormat = "MMM d";
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
@@ -59,6 +56,7 @@ public class CalenderUtil {
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
         return dateFormatter.format(date);
     }
+
     public static String getDay(Date date) {
         String dateFormat = "EEEE";
         SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
@@ -66,31 +64,10 @@ public class CalenderUtil {
     }
 
     public static String getCurrentTimeZone() {
-        //get Calendar instance
         Calendar now = Calendar.getInstance();
-
-        //get current TimeZone using getTimeZone method of Calendar class
         TimeZone timeZone = now.getTimeZone();
         return timeZone.getID();
     }
-
-   /* public static List<CalendarDay> getAllDisabledDay(Date date) {
-
-        List<CalendarDay> allDays = new ArrayList<>();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        int myMonth = cal.get(Calendar.MONTH);
-
-        while (myMonth == cal.get(Calendar.MONTH) || myMonth == cal.get(Calendar.MONTH + 1)) {
-            if (cal.get(Calendar.DATE) < date.getDate() || (cal.get(Calendar.DATE) > date.getDate() + 13)) {
-                CalendarDay calendarDay = CalendarDay.from(cal);
-                allDays.add(calendarDay);
-            }
-            cal.add(Calendar.DAY_OF_MONTH, 1);
-        }
-        return allDays;
-    }*/
 
     public static List<LinkedHashMap<String, Schedule>> getAllDays(Date date, List<Schedule> scheduleList) {
 
@@ -141,11 +118,6 @@ public class CalenderUtil {
         return df.format(date);
     }
 
-    public static String getDateOnlyFromDate(Date date) {
-        DateFormat df = new SimpleDateFormat("dd", Locale.ENGLISH);
-        return df.format(date);
-    }
-
     public static HashMap<Integer, Integer> getAllDatesFromList(List<Schedule> scheduleList) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         int i = 0;
@@ -158,28 +130,23 @@ public class CalenderUtil {
         return hashMap;
     }
 
-
-    //1 minute = 60 seconds
-//1 hour = 60 x 60 = 3600
-//1 day = 3600 x 24 = 86400
     public static boolean getDifferenceBetweenDates(Date startDate, Date endDate) {
-        //milliseconds
         long different = endDate.getTime() - startDate.getTime();
-        
+
         long secondsInMilli = 1000;
         long minutesInMilli = secondsInMilli * 60;
         long hoursInMilli = minutesInMilli * 60;
 
         long elapsedHours = Math.abs(different / hoursInMilli);
-        
 
-        if(elapsedHours < 4) {
+
+        if (elapsedHours < 4) {
             String currTime = getAMPMValueFromDate(startDate);
             String targetTime = getAMPMValueFromDate(endDate);
             return currTime.equalsIgnoreCase(targetTime);
         }
 
-        return  false;
+        return false;
     }
 
 
